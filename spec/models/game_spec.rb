e8b9558421a92817aa6716144276b4743d279db0
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2021-03-16 21:14:25 -0400

    Add tests to models

---
+++ spec/models/game_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+# Test suite for the Game model
+RSpec.describe Game, type: :model do
+  # Association test
+  # ensure Game model has a 1:m relationship with the Favourites model
+  it { should have_many(:favourites).dependent(:destroy) }
+  # ensure Game model has a m:m relationship with the User model
+  it { should have_many(:users) }
+  # Validation tests
+  # ensure columns title, description, image, and genre are present before saving
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:image) }
+  it { should validate_presence_of(:genre) }
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+# Test suite for the User model
+RSpec.describe User, type: :model do
+  # Association test
+  # ensure User model has a 1:m relationship with the Favourites model
+  it { should have_many(:favourites).dependent(:destroy) }
+  # ensure User model has a m:m relationship with the Game model
+  it { should have_many(:games) }
+  # Validation tests
+  # ensure column username is present before saving
+  it { should validate_presence_of(:username) }
+end

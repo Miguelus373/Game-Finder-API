41678cd7d720380b3c416326607024f8894bbbda
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2021-03-18 12:24:27 -0400

    Add routing tests

---
+++ spec/routing/games_routing_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe GamesController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(:get => "/games").to route_to("games#index")
+    end
+
+    it "routes to #show" do
+      expect(:get => "/games/1").to route_to("games#show", :id => "1")
+    end
+  end
+end
